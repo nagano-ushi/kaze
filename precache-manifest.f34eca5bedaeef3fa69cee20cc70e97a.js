85f83f7d524718c135e60b0ce8313233ceef5bb3
mesh_fukuda <fukuda@skeiei-11.alps.pref.nagano.lg.jp>
2024-01-22 16:04:17 +0900

    first commit

---
+++ precache-manifest.f34eca5bedaeef3fa69cee20cc70e97a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "40cdd969ae3b0c4154de",
+    "url": "/kaze/static/css/main.7ccf35ff.chunk.css"
+  },
+  {
+    "revision": "40cdd969ae3b0c4154de",
+    "url": "/kaze/static/js/main.40cdd969.chunk.js"
+  },
+  {
+    "revision": "1aec539e6f30aede219e",
+    "url": "/kaze/static/js/1.1aec539e.chunk.js"
+  },
+  {
+    "revision": "32d12fae82236f940eb3",
+    "url": "/kaze/static/js/runtime~main.32d12fae.js"
+  },
+  {
+    "revision": "b5500170e8d989ce4c26ea61794ae995",
+    "url": "/kaze/index.html"
+  }
+];
